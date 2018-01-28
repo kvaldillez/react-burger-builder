3db845eb8061bc646cb7c3c4ae22d8e923bbc2c9
Kyle Valdillez <kvaldillez@users.noreply.github.com>
2018-01-27 22:41:19 -0800

    Added an order summary modal, toolbar and the beginnigs of a mobile slide out menu

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,38 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import classes from './BurgerIngredient.css';
 
-const burgerIngredient = (props) => {
-    let ingredient = null;
+class BurgerIngredient extends Component{
+    render() {
+        let ingredient = null;
 
-    switch(props.type){
-        case('bread-bottom'):
-            ingredient = <div className={classes.BreadBottom}></div>;
+        switch (this.props.type) {
+          case "bread-bottom":
+            ingredient = <div className={classes.BreadBottom} />;
             break;
-        case('bread-top'):
-            ingredient = (
-                <div className={classes.BreadTop}>
-                    <div className={classes.Seeds1}></div>
-                    <div className={classes.Seeds2}></div>
-                </div>
-            );
+          case "bread-top":
+            ingredient = <div className={classes.BreadTop}>
+                <div className={classes.Seeds1} />
+                <div className={classes.Seeds2} />
+              </div>;
             break;
-        case('meat'):
-            ingredient = <div className={classes.Meat}></div>;
+          case "meat":
+            ingredient = <div className={classes.Meat} />;
             break;
-        case('cheese'):
-            ingredient = <div className={classes.Cheese}></div>;
+          case "cheese":
+            ingredient = <div className={classes.Cheese} />;
             break;
-        case('bacon'):
-            ingredient = <div className={classes.Bacon}></div>;
+          case "bacon":
+            ingredient = <div className={classes.Bacon} />;
             break;
-        case('salad'):
-            ingredient = <div className={classes.Salad}></div>;
+          case "salad":
+            ingredient = <div className={classes.Salad} />;
             break;
-        default:
+          default:
             ingredient = null;
-    }
+        }
 
-    return ingredient;
+        return ingredient;
+    }    
 }
 
-export default burgerIngredient;
+BurgerIngredient.propTypes = {
+    type: PropTypes.string.isRequired
+};
+
+export default BurgerIngredient;
---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Logo from '../../Logo/Logo';
+import classes from './Toolbar.css';
+import NavigationItems from '../NavigationItems/NavigationItems';
+
+const toolbar = (props) => {
+    return(
+        <header className={classes.Toolbar}>
+            <div>MENU</div>
+            <div className={classes.Logo}>
+                <Logo />
+            </div>
+            <nav>
+                <NavigationItems />
+            </nav>
+        </header>
+    );
+};
+
+export default toolbar;
